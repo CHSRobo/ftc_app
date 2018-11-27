3874f8d7d018a2de40c44941e6dbb882ea5a0a85
Will Archer <bwillarcher7@gmail.com>
2018-11-26 19:13:27 -0500

    Created Fortonomous.java

---
+++ Team11383/src/main/java/org/firstinspires/ftc/team11383/Fortonomous.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.team11383;
+
+/**
+ * Created by robotics on 10/20/2018.
+ */
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(name="Fortonomous")
+public class Fortonomous extends LinearOpMode{
+    private HardwareFortissimus2 robot = new HardwareFortissimus2();
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+    
+    
